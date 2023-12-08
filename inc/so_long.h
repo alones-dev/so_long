bded35aa74ccf0418c7e5d582e4d4cde6dbb8ca5
Kylian Daumont <kdaumont@bess-f3r3s8.clusters.42paris.fr>
2023-12-08 16:43:58 +0100

    advance

--- inc/so_long.h
+++ inc/so_long.h
@@ -6,18 +6,25 @@
 /*   By: kdaumont <kdaumont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 09:35:54 by kdaumont          #+#    #+#             */
-/*   Updated: 2023/12/08 10:29:45 by kdaumont         ###   ########.fr       */
+/*   Updated: 2023/12/08 16:38:43 by kdaumont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include <fcntl.h>
-# include <math.h>
-# include <mlx.h>
 # include "ft_printf.h"
 # include "get_next_line.h"
 # include "libft.h"
+# include <fcntl.h>
+# include <math.h>
+# include <mlx.h>
+
+typedef struct s_vars
+{
+	void	*mlx;
+	void	*win;
+	int		count;
+}			t_vars;
 
 #endif
--- srcs/test.c
+++ srcs/test.c
@@ -6,24 +6,53 @@
 /*   By: kdaumont <kdaumont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 09:54:18 by kdaumont          #+#    #+#             */
-/*   Updated: 2023/12/08 10:56:35 by kdaumont         ###   ########.fr       */
+/*   Updated: 2023/12/08 16:38:46 by kdaumont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+int	move_key(int keycode)
+{
+	if (keycode == 'w')
+		return (1);
+	if (keycode == 'a')
+		return (1);
+	if (keycode == 's')
+		return (1);
+	if (keycode == 'd')
+		return (1);
+	return (0);
+}
+
+int	create_trgb(int t, int r, int g, int b)
+{
+	return (t << 24 | r << 16 | g << 8 | b);
+}
+
+int	close_win(int keycode, t_vars *vars)
+{
+	if (keycode == 65307)
+	{
+		mlx_destroy_window(vars->mlx, vars->win);
+		return (1);
+	}
+	if (move_key(keycode))
+	{
+		vars->count = vars->count + 1;
+		ft_printf("move: %d\n", vars->count);
+		return (1);
+	}
+	return (0);
+}
+
 int	main(void)
 {
-	void *mlx;
-	void *mlx_win;
+	t_vars	vars;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello World!");
-	if (mlx_win)
-		ft_printf("1\n");
-	else
-		ft_printf("2\n");
-	mlx_loop(mlx);
-
-	return (0);
+	vars.count = 0;
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Hello world!");
+	mlx_key_hook(vars.win, close_win, &vars);
+	mlx_loop(vars.mlx);
 }
